e9221dc2a14a2e20fd53388c4e70be44892b444e
vishesh <visheshtiwari@ggx.io>
2023-09-30 12:13:45 +0530

    added new form user and sort by last name

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -41,13 +43,11 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 		return "employees/employee-form";
 	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		employeeService.save(theEmployee);
+
+		return "redirect:/employees/list";
+	}
 }
-
-
-
-
-
-
-
-
-
