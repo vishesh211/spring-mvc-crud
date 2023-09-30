69a5ed12d63c6abde2f8b922a539eeb329c89ae5
vishesh <visheshtiwari@ggx.io>
2023-09-30 14:07:23 +0530

    added update functionality

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +38,15 @@ public class EmployeeController {
 		//create the model attribute
 		Employee theEmployee = new Employee();
 		theModel.addAttribute("employee", theEmployee);
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+		Employee theEmployee = employeeService.findById(theId);
+
+		theModel.addAttribute("employee", theEmployee);
+
 		return "employees/employee-form";
 	}
 
